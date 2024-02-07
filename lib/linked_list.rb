19da3b26c73ab197d7cb4e19a4942f1dae1f5aa4
Jared Hobson <jaredmhobson@gmail.com>
2024-02-06 20:53:39 -0800

    Add completed linked list class and test

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,10 +7,6 @@ class LinkedList
     @head = nil
   end
 
-  # def append(data)
-  #   @head = Node.new(data)
-  # end
-
   def append(data)
     if @head == nil
       @head = Node.new(data)
@@ -42,5 +38,71 @@ class LinkedList
     end
     word_array.join(" ")
   end
-end
 
+  def prepend(data)
+    if @head == nil
+      @head = Node.new(data)
+    else
+      old_head = @head
+      @head = Node.new(data)
+      @head.next_node = old_head
+    end
+  end
+
+  def insert(position, data)
+    if @head == nil
+      @head = Node.new(data)
+    else
+      current = @head
+      previous = current
+      new_node = Node.new(data)
+      counter = 0
+      until counter == position
+        counter += 1
+        previous = current
+        current = current.next_node
+      end
+      previous.next_node = new_node
+      new_node.next_node = current
+    end
+  end
+
+  def find(first_position, return_amount)
+    current = @head
+    counter = 0
+    word_array = []
+      until counter == first_position
+        counter += 1
+        current = current.next_node
+      end
+      current_counter = counter
+      until counter == current_counter + return_amount
+        word_array << current.data
+        counter += 1
+        current = current.next_node
+      end
+      word_array.join(" ")
+  end
+
+  def includes?(data)
+    current = @head
+    until current.next_node == nil
+      if current.data == data
+        return true
+      else
+        return false
+      end
+      current = current.next_node
+    end
+  end
+
+  def pop
+    current = @head
+    until current.next_node.next_node == nil
+      current = current.next_node
+    end
+    last_node_data = current.next_node.data
+    current.next_node = nil
+    last_node_data
+  end
+end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -42,4 +42,92 @@ RSpec.describe LinkedList do
 
     expect(list.to_string).to eq('doop deep')
   end
+
+  it 'can change the head while maintaining the existing list' do
+    list = LinkedList.new
+
+    list.append("plop")
+
+    expect(list.to_string).to eq('plop')
+
+    list.append("suu")
+
+    expect(list.to_string).to eq('plop suu')
+
+    list.prepend("dop")
+
+    expect(list.to_string).to eq('dop plop suu')
+  end
+
+  it 'can insert a node into any position while maintaining the rest of the node' do
+    list = LinkedList.new
+
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+
+    expect(list.to_string).to eq('dop plop suu')
+
+    list.insert(1, "woo")
+
+    expect(list.to_string).to eq('dop woo plop suu')
+  end
+
+  it 'will insert the node at the head if there are no nodes' do
+    list = LinkedList.new
+
+    list.insert(5, "woo")
+
+    expect(list.to_string).to eq('woo')
+  end
+
+  it 'can find a node at a certain position and a range after' do
+    list = LinkedList.new
+
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.find(2, 1)).to eq('shi')
+
+    expect(list.find(1, 3)).to eq('woo shi shu')
+  end
+
+    it 'can check if the linked list includes a node with specific data' do
+    list = LinkedList.new
+
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.includes?('deep')).to be true
+  end
+
+  it 'can remove the last node and return its data' do
+    list = LinkedList.new
+
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.to_string).to eq('deep woo shi shu blop')
+
+    expect(list.count).to eq(5)
+
+    expect(list.pop).to eq('blop')
+
+    expect(list.count).to eq(4)
+
+    expect(list.to_string).to eq ('deep woo shi shu')
+
+    expect(list.pop).to eq('shu')
+
+    expect(list.to_string).to eq ('deep woo shi')
+  end
 end
